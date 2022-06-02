6aa740081246e8eacc23629c54623bfdbbdc9e03
Patz Paul <pathefister@gmail.com>
2022-06-02 05:12:26 +0300

    had to brush on a few lines and add extra code

--- rusty/guessing_game/src/main.rs
+++ rusty/guessing_game/src/main.rs
@@ -1,3 +1,4 @@
+
 use std::io;
 use std::cmp::Ordering;
 use rand::{thread_rng, Rng};
@@ -18,11 +19,12 @@ fn main() {
         println!("Mmmh...You Guessed: {}", guess);
         let secret_number: u32 = rng.gen_range(1..10);
 //To parse guess so it matches the secret number data value
-    //let guess: u32 = guess.trim().parse().expect("Please type a number!");
+//let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
         let parse_result = guess.trim().parse();
             // Destructure the result
         let guess: u32 = match parse_result {
+//them enum boys
 // If parse succeeded evaluate to the number
             Ok(num) => num,
 // If parse failed 
@@ -31,7 +33,9 @@ fn main() {
 //If Else Case To Have A Final Say
         if secret_number == guess {
            println!("Wow Bruv :) You Guessed Correctly!{} {}",secret_number ,guess);
-        } 
+           break;
+        }
+
         else {
             println!("HaHa! You Guessed Wrong :( {} {}",secret_number ,guess);
             match guess.cmp(&secret_number){
@@ -39,7 +43,7 @@ fn main() {
                 Ordering::Greater => println!("So Close But Too High"),
                 Ordering::Equal   => {
                     println!("You Are One Badass Sharpshooter");
-                    break;
+                    
                 }
 
             }
